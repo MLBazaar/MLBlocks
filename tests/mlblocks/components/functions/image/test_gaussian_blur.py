b7de59d391186ee4eab0629e44e016f849bca1b9
Carles Sala <c.sala.stq@gmail.com>
2018-05-10 19:13:20 +0200

    Fix flake8 complain

--- tests/mlblocks/components/functions/image/test_gaussian_blur.py
+++ tests/mlblocks/components/functions/image/test_gaussian_blur.py
@@ -1,4 +1,4 @@
-from mlblocks.components.functions.image.gaussian_blur import gaussian_blur_fnc
+# from mlblocks.components.functions.image.gaussian_blur import gaussian_blur_fnc
 
 
 def test_gaussian_blur_fnc():
