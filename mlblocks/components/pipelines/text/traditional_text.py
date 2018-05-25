437b16f48ea9f86753a5062f1f92a94dc95aa090
Carles Sala <c.sala.stq@gmail.com>
2018-05-25 15:15:16 +0200

    Styling change

--- mlblocks/components/pipelines/text/traditional_text.py
+++ mlblocks/components/pipelines/text/traditional_text.py
@@ -3,9 +3,5 @@ from mlblocks.ml_pipeline.ml_pipeline import MLPipeline
 
 class TraditionalTextPipeline(MLPipeline):
     """Traditional text pipeline."""
-    BLOCKS = [
-        'count_vectorizer',
-        'to_array',
-        'tfidf_transformer',
-        'multinomial_nb',
-    ]
+
+    BLOCKS = ['count_vectorizer', 'to_array', 'tfidf_transformer', 'multinomial_nb']
