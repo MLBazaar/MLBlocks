636940731433471473d0fee12e4890274720ff1b
Carles Sala <c.sala.stq@gmail.com>
2018-06-01 13:15:28 -0400

    Remove pipelines and examples, now in MLBlocks-Demos

--- mlblocks/components/pipelines/image/traditional_image.py
+++
@@ -1,7 +0,0 @@
-from mlblocks.ml_pipeline.ml_pipeline import MLPipeline
-
-
-class TraditionalImagePipeline(MLPipeline):
-    """Traditional image pipeline using HOG features."""
-
-    BLOCKS = ['HOG', 'random_forest_classifier']
--- mlblocks/components/pipelines/text/traditional_text.py
+++
@@ -1,7 +0,0 @@
-from mlblocks.ml_pipeline.ml_pipeline import MLPipeline
-
-
-class TraditionalTextPipeline(MLPipeline):
-    """Traditional text pipeline."""
-
-    BLOCKS = ['count_vectorizer', 'to_array', 'tfidf_transformer', 'multinomial_nb']
