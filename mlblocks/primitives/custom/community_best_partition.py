2a95dfead32643be45886a1d8e606a379f26131d
Carles Sala <c.sala.stq@gmail.com>
2018-06-17 20:26:34 +0200

    Use predict_params instead of init_params

--- mlblocks/primitives/custom/community_best_partition.py
+++ mlblocks/primitives/custom/community_best_partition.py
@@ -5,17 +5,12 @@ import community as co
 
 class CommunityBestPartition(object):
 
-    def __init__(self, partition=None, graph=None):
-        if partition is None and graph is None:
-            raise ValueError("Either partition or graph must be provided")
-
-        self.partition = partition or co.best_partition(graph)
-
     def fit(self, X, y):
         pass
 
-    def produce(self, X):
-        values = [b for a, b in self.partition.items()]
+    def produce(self, X, partition=None, grap=None):
+        partition = partition or co.best_partition(graph)
+        values = [b for a, b in partition.items()]
         missing_community_index = np.max(values) + 10
 
         result = pd.Series(index=X.index)
@@ -23,11 +18,11 @@ class CommunityBestPartition(object):
         for i in X.index:
             node = X.loc[i][0]
 
-            if node in self.partition:
-                community = self.partition[node]
+            if node in partition:
+                community = partition[node]
 
-            elif str(node) in self.partition:
-                community = self.partition[str(node)]
+            elif str(node) in partition:
+                community = partition[str(node)]
 
             else:
                 community = missing_community_index
