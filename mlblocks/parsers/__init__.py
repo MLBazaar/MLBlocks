9fb9fd4fb398f00bef022c9e47d25267ee0cf96d
Carles Sala <c.sala.stq@gmail.com>
2018-07-11 15:09:09 -0400

    Cleanup unused things

--- mlblocks/parsers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import importlib
-
-
-def import_object(object_name):
-    """Import an object from its Fully Qualified Name."""
-    package, name = object_name.rsplit('.', 1)
-    return getattr(importlib.import_module(package), name)
