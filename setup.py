5fde62f73312752b5abee12e1f56572644b12684
Carles Sala <c.sala.stq@gmail.com>
2018-07-12 17:37:53 -0400

    Remove unneeded dependencies

--- setup.py
+++ setup.py
@@ -5,36 +5,29 @@
 
 from setuptools import find_packages, setup
 
+
 with open('README.md') as readme_file:
     readme = readme_file.read()
+
 
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
+
 install_requires = [
 ]
+
 
 tests_require = [
     'mock>=2.0.0',
     'pytest>=3.4.2',
 ]
 
-primitives_require = [
-    'Keras>=2.1.6',
-    'featuretools>=0.1.17',
-    'lightfm>=1.15',
-    'networkx>=2.0',
-    'numpy>=1.14.0',
-    'opencv-python>=3.4.0.12',
-    'python-louvain>=0.10',
-    'scikit-image>=0.13.1',
-    'scikit-learn>=0.19.1',
-    'scipy>=1.1.0',
-]
 
 setup_requires = [
     'pytest-runner>=2.11.1',
 ]
+
 
 setup(
     author='MIT Data To AI Lab',
@@ -51,11 +44,10 @@ setup(
     description="Pipelines and primitives for machine learning and data science.",
     extras_require={
         'test': tests_require,
-        'primitives': primitives_require
     },
     include_package_data=True,
     install_requires=install_requires,
-    keywords='machine learning classification',
+    keywords='auto machine learning classification regression data science pipeline',
     license="MIT license",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
