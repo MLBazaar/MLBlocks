b34dcaac9c04330a92a41a2c024d7e9acc09e29a
Carles Sala <c.sala.stq@gmail.com>
2018-07-13 11:58:01 -0400

    Bump version: 0.1.7-dev → 0.1.7

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/MLBlocks',
-    version='0.1.7-dev',
+    version='0.1.7',
     zip_safe=False,
 )
