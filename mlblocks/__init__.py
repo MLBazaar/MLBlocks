232615349bfa06348196bc2e93d40a1901a06177
Carles Sala <c.sala.stq@gmail.com>
2018-08-17 18:54:26 +0200

    Bump version: 0.2.0 → 0.2.1-dev

--- mlblocks/__init__.py
+++ mlblocks/__init__.py
@@ -10,7 +10,7 @@ from mlblocks.mlpipeline import MLPipeline
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com',
-__version__ = '0.2.0'
+__version__ = '0.2.1-dev'
 
 __all__ = [
     'MLBlock', 'MLPipeline', 'PRIMITIVES_PATHS',
