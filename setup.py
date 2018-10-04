2f38b734701411a028081bfca8dfd8d9816e0151
Carles Sala <carles@pythiac.com>
2018-10-04 17:51:28 -0400

    Bump version: 0.2.3-dev → 0.2.3

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/MLBlocks',
-    version='0.2.3-dev',
+    version='0.2.3',
     zip_safe=False,
 )
