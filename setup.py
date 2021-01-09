04bb5fc72f55a9e2f439bed2d4ec3ae6537f52f4
Carles Sala <carles@pythiac.com>
2021-01-09 17:22:42 +0100

    Bump version: 0.4.0.dev1 → 0.4.0

--- setup.py
+++ setup.py
@@ -114,6 +114,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/MLBazaar/MLBlocks',
-    version='0.4.0.dev1',
+    version='0.4.0',
     zip_safe=False,
 )
