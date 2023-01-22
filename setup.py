a70b30713416ca1bc1a4cf2c2675cda383e28ca8
sarahmish <sarahalnegheimish@gmail.com>
2023-01-22 16:26:18 -0500

    Bump version: 0.5.0 → 0.5.1.dev0

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/MLBazaar/MLBlocks',
-    version='0.5.0',
+    version='0.5.1.dev0',
     zip_safe=False,
 )
