b85983d956699c5863e153816543fb6f29bdb8ff
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2023-04-14 14:28:08 -0400

    Bump version: 0.5.1.dev0 → 0.5.1.dev1

--- mlblocks/__init__.py
+++ mlblocks/__init__.py
@@ -20,7 +20,7 @@ __author__ = 'MIT Data To AI Lab'
 __copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
 __license__ = 'MIT'
-__version__ = '0.5.1.dev0'
+__version__ = '0.5.1.dev1'
 
 __all__ = [
     'MLBlock',
--- setup.py
+++ setup.py
@@ -120,6 +120,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/MLBazaar/MLBlocks',
-    version='0.5.1.dev0',
+    version='0.5.1.dev1',
     zip_safe=False,
 )
